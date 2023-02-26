f2e9c1fecb8d08c6f1653bf0d55de644beeda51b
RhazzXIX <jm21tonio@gmail.com>
2023-02-26 13:39:13 +0800

    Add ships that will be used in the game

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -2,6 +2,10 @@ import Ship from "./ship";
 
 const Gameboard = () => {
   const gameboard = [];
+  const commander = Ship('Commander', 5);
+  const battleship = Ship('Battleship', 4);
+  const destroyer = Ship('Destroyer', 3);
+  const submarine = Ship('Submarine', 3)
   
   for (let i = 0; i < 10; i += 1) {
     const row = [];
@@ -24,6 +28,10 @@ const Gameboard = () => {
     })
     return displayedGameboard;
   };
+
+  const placeShip = () => {
+
+  }
 
   
   return { showGameboard };
