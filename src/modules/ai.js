1f69f42d577b468663f841911153cd5ae24242c1
RhazzXIX <jm21tonio@gmail.com>
2023-02-28 16:28:34 +0800

    Prevent A.I. to attack a grid twice.
    Refactored enterCoords logic to check if a generated coordinates was
    already attacked.
    Added attackedCoordsEntered array to hold coordinates that were already
    attack.
    Created checkCoordinates function to check if a coordinate was already
    entered.

--- src/modules/ai.js
+++ src/modules/ai.js
@@ -1,15 +1,40 @@
 import Gameboard from "./gameboard";
 
 const commanderAI = () => {
+  const attackCoordsEntered = [];
+
   const generateCoords = () => {
     const x = Math.floor(Math.random() * 10);
     const y = Math.floor(Math.random() * 10);
     return [x, y];
   };
 
+  const checkCoordinates = (coords) => {
+    let entered = false;
+    const [x, y] = [...coords];
+    const coordsCopy = [];
+    attackCoordsEntered.forEach((entry) => coordsCopy.push(entry));
+    coordsCopy.forEach((entry, i, arr) => {
+      const [a, b] = [...entry];
+      if (a === x && b === y) {
+        entered = true;
+        arr.splice(i);
+      }
+    });
+
+    return entered;
+  };
+
   const enterCoords = (fn) => {
+    if (attackCoordsEntered.length === 100) return;
     const coords = generateCoords();
-    fn(coords);
+    const entered = checkCoordinates(coords);
+    if (!entered) {
+      attackCoordsEntered.push(coords);
+      fn(coords);
+    } else if (entered) {
+      enterCoords(fn);
+    }
   };
 
   return { enterCoords };
--- src/test/ai.test.js
+++ src/test/ai.test.js
@@ -3,8 +3,27 @@
 /* eslint-disable no-undef */
 import commanderAI from "../modules/ai";
 
-const placeShip = jest.fn();
-const attackMock = jest.fn((x) => x);
+const coordinates = [];
+const attackMock = jest.fn((x) => coordinates.push(x));
+const checkCoordinates = (x) => {
+  const [y, z] = [...x];
+  const lastCoordIndex = coordinates.findIndex((coords) => {
+    const [a, b] = [...coords];
+    if (a === y && b === z) return coords;
+  });
+  coordinates.splice(lastCoordIndex, 1);
+  let double = false;
+  const coordsCopy = [];
+  coordinates.forEach((coords) => coordsCopy.push(coords));
+  coordsCopy.forEach((coords, i, arr) => {
+    const [a, b] = [...coords];
+    if (a === y && b === z) {
+      double = true;
+      arr.splice(i);
+    }
+  });
+  return double;
+};
 
 const playerAI = commanderAI();
 
@@ -18,27 +37,136 @@ describe("A.I. can use receiveAttack function properly", () => {
     expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
   });
 
-  test("A.I. will enter different coordinates within the board", () => {
-    playerAI.enterCoords(attackMock);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
-    playerAI.enterCoords(attackMock);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
-    playerAI.enterCoords(attackMock);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
-    expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
+  describe("Coordinates entered by A.I.", () => {
+    beforeEach(() => coordinates.splice());
+
+    test("A.I. will enter coordinates within the board", () => {
+      playerAI.enterCoords(attackMock);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
+      playerAI.enterCoords(attackMock);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
+      playerAI.enterCoords(attackMock);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][0]).not.toBeGreaterThan(9);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeLessThan(0);
+      expect(attackMock.mock.calls[0][0][1]).not.toBeGreaterThan(9);
+    });
+
+    test("A.I will not repeat coordinates", () => {
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      playerAI.enterCoords(attackMock);
+      expect(checkCoordinates(attackMock.mock.calls[0][0])).toBeFalsy();
+    });
   });
 });
 
 test.skip("A.I. can set a ship in the gameBoard", () => {
-  expect(commanderAI.showGameBoard()).not.toEqual([
+  expect(playerAI.showGameBoard()).not.toEqual([
     [
       { shot: null },
       { shot: null },
