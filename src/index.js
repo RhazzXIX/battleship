c747f9722ea86adcc6fcf2f1b58d5ee59ccf114c
RhazzXIX <jm21tonio@gmail.com>
2023-03-01 20:15:08 +0800

    Lay out html ship placement section.
    Layed out initial structure and style of player's ship placement
    section.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import './style.css';
+import './index.html';
+import controlDOM from './modules/DOMcontrol';
 
 console.log('Hello Up-skilling World!')
--- src/modules/DOMcontrol.js
+++ src/modules/DOMcontrol.js
@@ -1,6 +1,22 @@
 
-const controlDOM = () => {
+const controlDOM = (() => {
+  const main = document.querySelector('main');
+  const startSection = main.querySelector('section#start');
+  const playerForm = startSection.querySelector('form#player');
+  const startBtn = startSection.querySelector('button');
+  const placeSection = main.querySelector('section#place');
+  const btn = placeSection.querySelector('button');
 
-}
+  const removeForm = () => {
+    main.removeChild(startSection);
+  }
+
+  btn.addEventListener('click', () => {
+    main.appendChild(startSection);
+  })
+
+  startBtn.addEventListener('click', removeForm)
+  main.removeChild(startSection);
+}) () 
 
 export default controlDOM
