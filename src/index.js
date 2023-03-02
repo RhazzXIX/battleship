0e5df23f734e03a1d152efc60acf00b5fcef3283
RhazzXIX <jm21tonio@gmail.com>
2023-03-02 10:13:25 +0800

    Lay out game section html structure & style

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import './style.css';
-import './index.html';
-import controlDOM from './modules/DOMcontrol';
+import "./style.css";
+import "./index.html";
+import controlDOM from "./modules/DOMcontrol";
 
-console.log('Hello Up-skilling World!')
+console.log("Hello Up-skilling World!");
--- src/modules/DOMcontrol.js
+++ src/modules/DOMcontrol.js
@@ -1,22 +1,22 @@
-
 const controlDOM = (() => {
-  const main = document.querySelector('main');
-  const startSection = main.querySelector('section#start');
-  const playerForm = startSection.querySelector('form#player');
-  const startBtn = startSection.querySelector('button');
-  const placeSection = main.querySelector('section#place');
-  const btn = placeSection.querySelector('button');
+  const main = document.querySelector("main");
+  const startSection = main.querySelector("section#start");
+  const playerForm = startSection.querySelector("form#player");
+  const startBtn = startSection.querySelector("button");
+  const placeShipSection = main.querySelector("section#place");
+  const btn = placeShipSection.querySelector("button");
 
   const removeForm = () => {
     main.removeChild(startSection);
-  }
+  };
 
-  btn.addEventListener('click', () => {
+  btn.addEventListener("click", () => {
     main.appendChild(startSection);
-  })
+  });
 
-  startBtn.addEventListener('click', removeForm)
+  startBtn.addEventListener("click", removeForm);
   main.removeChild(startSection);
-}) () 
+  main.removeChild(placeShipSection);
+})();
 
-export default controlDOM
+export default controlDOM;
