75783a8123286c2e35437a6dc18aac10159bdb37
RhazzXIX <jm21tonio@gmail.com>
2023-03-03 17:49:29 +0800

    Add DOMcontrol module for controlling html nodes
    Selected initial nodes and hid other html sections.

--- src/modules/DOMcontrol.js
+++ src/modules/DOMcontrol.js
@@ -1,22 +1,30 @@
 const controlDOM = (() => {
+  const body = document.querySelector("body");
+
   const main = document.querySelector("main");
   const startSection = main.querySelector("section#start");
   const playerForm = startSection.querySelector("form#player");
   const startBtn = startSection.querySelector("button");
+
   const placeShipSection = main.querySelector("section#place");
-  const btn = placeShipSection.querySelector("button");
+  const axisBtn = placeShipSection.querySelector("button#axis");
+  const placeShipBoard = placeShipSection.querySelector("div#plBoard");
 
-  const removeForm = () => {
-    main.removeChild(startSection);
-  };
+  const gameSection = main.querySelector("section#game");
+  const gamePlBoard = gameSection.querySelector("div#gamePlBoard");
+  const gameCompBoard = gameSection.querySelector("div#gameCompBoard");
 
-  btn.addEventListener("click", () => {
-    main.appendChild(startSection);
-  });
+  const noticeSection = body.querySelector("section#notice");
+  const winnerNotice = noticeSection.querySelector("h2#announce");
+  const restartBtn = noticeSection.querySelector("button#restart");
 
-  startBtn.addEventListener("click", removeForm);
-  main.removeChild(startSection);
+  // main.removeChild(startSection);
   main.removeChild(placeShipSection);
+  placeShipBoard.classList.remove("hidden");
+  main.removeChild(gameSection);
+  gameSection.classList.remove("hidden");
+  body.removeChild(noticeSection);
+  noticeSection.classList.remove("hidden");
 })();
 
 export default controlDOM;
