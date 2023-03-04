fe62773493b6ebaf32f4092b7c597783bd44744b
RhazzXIX <jm21tonio@gmail.com>
2023-03-04 13:44:00 +0800

    Add methods to player factory
    Created a variable "turn" which stores the value of the player's turn.
    Created showTurn method which returns the "turn" value.
    Created attackBoard method for attacking the board of the enemy if it's
    the player's turn, and for starting the "turn" of the enemy.
    Created the startTurn method to which makes the player's turn variable
    to true.

--- src/modules/factories/player.js
+++ src/modules/factories/player.js
@@ -1,10 +1,24 @@
-
 const Player = (name) => {
   const playerName = name;
 
+  let turn = true;
+
   const showName = () => playerName;
 
-  return { showName }
-}
+  const showTurn = () => turn;
 
-export default Player
+  const attackBoard = (coords, board, enemyTurn) => {
+    if (turn === false) return;
+    board(coords);
+    turn = false;
+    enemyTurn();
+  };
+
+  const startTurn = () => {
+    turn = true;
+  };
+
+  return { showName, showTurn, attackBoard, startTurn };
+};
+
+export default Player;
--- src/test/player.test.js
+++ src/test/player.test.js
@@ -2,8 +2,51 @@
 /* eslint-disable no-undef */
 import Player from "../modules/factories/player";
 
-let player
-beforeEach(() => (player = Player('test')))
+let player;
+
+let enemyBoard;
+let enemyTurn;
+const coord = [0, 1];
+
+beforeEach(() => {
+  player = Player("test");
+  enemyBoard = jest.fn((array) => array);
+  enemyTurn = jest.fn();
+});
+
 test("Player can show their name", () => {
-  expect(player.showName()).toEqual('test');
-})
+  expect(player.showName()).toEqual("test");
+});
+
+test("Player can show their turn", () => {
+  expect(player.showTurn()).toEqual(true);
+});
+
+test("Player can attack on boards, end their turn and call enemy turn", () => {
+  expect(player.showTurn()).toEqual(true);
+  player.attackBoard(coord, enemyBoard, enemyTurn);
+  expect(enemyBoard.mock.calls).toHaveLength(1);
+  expect(enemyBoard.mock.calls[0][0]).toEqual(coord);
+  expect(enemyTurn.mock.calls).toHaveLength(1);
+  expect(player.showTurn()).toEqual(false);
+});
+
+test(`Player cannot attack if it's not their turn`, () => {
+  expect(player.showTurn()).toEqual(true);
+  player.attackBoard(coord, enemyBoard, enemyTurn);
+  expect(enemyBoard.mock.calls).toHaveLength(1);
+  expect(player.showTurn()).toEqual(false);
+  player.attackBoard(coord, enemyBoard, enemyTurn);
+  expect(enemyBoard.mock.calls).toHaveLength(1);
+});
+
+test("Player can have their turn", () => {
+  expect(player.showTurn()).toEqual(true);
+  player.attackBoard(coord, enemyBoard, enemyTurn);
+  expect(enemyBoard.mock.calls).toHaveLength(1);
+  expect(player.showTurn()).toEqual(false);
+  player.startTurn();
+  expect(player.showTurn()).toEqual(true);
+  player.attackBoard(coord, enemyBoard, enemyTurn);
+  expect(enemyBoard.mock.calls).toHaveLength(2);
+});
