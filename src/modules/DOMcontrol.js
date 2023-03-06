6af8b6ac9f3ffd9455715d5453d4a5b23ff41ccf
RhazzXIX <jm21tonio@gmail.com>
2023-03-07 00:48:39 +0800

    Lay out UI game flow of player name entry
    Added playerInput node.
    Created loadGame function for facilitating the DOM from entering the
    user's name up to loading the placing ship section.

--- src/modules/DOMcontrol.js
+++ src/modules/DOMcontrol.js
@@ -3,11 +3,13 @@ import Game from "./game";
 import parseGridCoords from "./doms/parseGridCoords";
 
 const controlDOM = (() => {
+  // DOM nodes
   const body = document.querySelector("body");
 
-  const main = document.querySelector("main");
+  const main = body.querySelector("main");
   const startSection = main.querySelector("section#start");
   const playerForm = startSection.querySelector("form#player");
+  const playerInput = playerForm.querySelector("input#pName");
   const startBtn = startSection.querySelector("button");
 
   const placeShipSection = main.querySelector("section#place");
@@ -22,19 +24,18 @@ const controlDOM = (() => {
   const winnerNotice = noticeSection.querySelector("h2#announce");
   const restartBtn = noticeSection.querySelector("button#restart");
 
-  main.removeChild(startSection);
+  // Initial Load
   main.removeChild(placeShipSection);
   placeShipSection.classList.remove("hidden");
-  // main.removeChild(gameSection);
+  main.removeChild(gameSection);
   gameSection.classList.remove("hidden");
   body.removeChild(noticeSection);
   noticeSection.classList.remove("hidden");
 
   const game = Game();
-  const boards = game.getGameBoard();
-  game.setPlayer("test");
-  attachDivGrid(gamePlBoard, boards.player, "player");
-  attachDivGrid(gameCompBoard, boards.comp);
+  let boards;
+
+  // Functions for DOM control
 
   const gridClickEvent = (index) => {
     const coord = parseGridCoords(index);
@@ -48,21 +49,43 @@ const controlDOM = (() => {
   function addGridClickEvent() {
     const compGrids = gameCompBoard.querySelectorAll("div.grid");
     compGrids.forEach((grid, j) => {
-      grid.addEventListener("click", () => {
+      grid.addEventListener("click", (e) => {
+        e.stopPropagation();
         gridClickEvent(j);
         addGridClickEvent();
       });
     });
   }
 
-  addGridClickEvent(
-    gameCompBoard,
-    gamePlBoard,
-    boards,
-    game.attack,
-    removeGrid,
-    attachDivGrid
-  );
+  function loadGame(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    game.setPlayer(playerInput.value);
+    boards = game.getGameBoard();
+    attachDivGrid(placeShipBoard, boards.player, "player");
+    main.removeChild(startSection);
+    main.appendChild(placeShipSection);
+    playerInput.value = "";
+  }
+
+  // Eventlisteners
+
+  playerForm.addEventListener("submit", loadGame);
+
+  // startBtn.addEventListener("click", startGame);
+
+  // game.setPlayer("test");
+  // attachDivGrid(gamePlBoard, boards.player, "player");
+  // attachDivGrid(gameCompBoard, boards.comp);
+
+  // addGridClickEvent(
+  //   gameCompBoard,
+  //   gamePlBoard,
+  //   boards,
+  //   game.attack,
+  //   removeGrid,
+  //   attachDivGrid
+  // );
 
   // setTimeout(() => {
   //   removeGrid(placeShipBoard);
